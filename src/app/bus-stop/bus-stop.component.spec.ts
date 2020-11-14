867d6a858e34aeb40d2fcb0d445f514b161c8664
dimarchi <32382129+dimarchi@users.noreply.github.com>
2020-11-14 19:38:23 +0200

    Update to Angular 11

--- src/app/bus-stop/bus-stop.component.spec.ts
+++ src/app/bus-stop/bus-stop.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BusStopComponent } from './bus-stop.component';
 
@@ -6,7 +6,7 @@ describe('BusStopComponent', () => {
   let component: BusStopComponent;
   let fixture: ComponentFixture<BusStopComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BusStopComponent ]
     })
