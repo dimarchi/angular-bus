b66e41c419ada7a50d0ae84b34a4bd73f6002317
dimarchi <32382129+dimarchi@users.noreply.github.com>
2022-01-21 19:32:38 +0200

    Update packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,7 +25,7 @@ export class AppComponent {
 
   // from https://stackoverflow.com/questions/11415106/issue-with-calculating-compass-bearing-between-two-gps-coordinates
   // modified
-  bearing(lat1,lng1,lat2,lng2) {
+  bearing(lat1: number,lng1: number,lat2: number,lng2: number) {
     var dLon = (lng2-lng1);
     var y = Math.sin(dLon) * Math.cos(lat2);
     var x = Math.cos(lat1)*Math.sin(lat2) - Math.sin(lat1)*Math.cos(lat2)*Math.cos(dLon);
@@ -33,11 +33,11 @@ export class AppComponent {
     return 360 - ((brng + 360) % 360);
     }
 
-   toDeg(rad) {
+   toDeg(rad: number) {
     return rad * 180 / Math.PI;
     }
 
-    getNearestBusStops(lat, lon) {
+    getNearestBusStops(lat: number, lon: number) {
 
         const DIGITRANSIT_URL = 'https://api.digitransit.fi/routing/v1/routers/finland/index/graphql';
 
