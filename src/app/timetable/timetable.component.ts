fdce00b5f66428974f84fa35e46481f8e5e81fc1
dimarchi <dimarchi@iki.fi>
2022-09-25 14:56:14 +0300

    Change from moment.js to Luxon

--- src/app/timetable/timetable.component.ts
+++ src/app/timetable/timetable.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, OnChanges } from '@angular/core';
-import * as moment from 'moment';
+import { DateTime } from 'luxon';
 import { DataService } from '../data.service';
 
 @Component({
@@ -12,9 +12,9 @@ export class TimetableComponent implements OnInit {
   @Input() stop: any = {};
   @Input() routeTimeTable: any = {};
   timetable: any = [];
-  selectedStop:any = {};
+  selectedStop: any = {};
 
-  constructor(private resetter : DataService) { }
+  constructor(private resetter: DataService) { }
 
   ngOnInit() {
   }
@@ -29,9 +29,9 @@ export class TimetableComponent implements OnInit {
   updateTimeTable(stopID) {
     const DIGITRANSIT_URL = 'https://api.digitransit.fi/routing/v1/routers/finland/index/graphql';
 
-      // query from https://digitransit.fi/en/developers/apis/1-routing-api/stops/
-      // Query scheduled departure and arrival times of a stop
-      const query = `
+    // query from https://digitransit.fi/en/developers/apis/1-routing-api/stops/
+    // Query scheduled departure and arrival times of a stop
+    const query = `
       {
         stop(id: "${stopID}") {
           name
@@ -51,29 +51,32 @@ export class TimetableComponent implements OnInit {
       }
       `;
 
-      fetch(DIGITRANSIT_URL, {
-        method: 'post',
-        headers: {
-            'Content-Type': 'application/graphql'
-        },
-        body: query
-      })
+    fetch(DIGITRANSIT_URL, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/graphql'
+      },
+      body: query
+    })
       .then(res => res.json())
       .then(data => {
         this.timetable = [];
-          if (data.data.stop) {
-            let time = data.data.stop.stoptimesWithoutPatterns;
+        if (data.data.stop) {
+          let time = data.data.stop.stoptimesWithoutPatterns;
 
-            time.map(t => {
-              let arrival = moment(t.serviceDay * 1000).seconds(t.realtimeArrival).format('HH:mm'); // formatting unix timestamp
-              let departure = moment(t.serviceDay * 1000).seconds(t.realtimeDeparture).format('HH:mm');
-              let scheduleArr = moment(t.serviceDay * 1000).seconds(t.scheduledArrival).format('HH:mm');
-              let scheduleDep = moment(t.serviceDay * 1000).seconds(t.scheduledDeparture).format('HH:mm');
-              let timeDate = new Date(t.serviceDay * 1000).toUTCString();
-              this.timetable = [...this.timetable, {'name': data.data.stop.name, 'headsign': t.headsign, 'realtimeArrival': t.realtimeArrival, 'realtimeDeparture': t.realtimeDeparture, 'scheduledArrival': scheduleArr, 'scheduledDeparture': scheduleDep, 'serviceDay': t.serviceDay, 'arrival': arrival, 'departure': departure, 'date': timeDate, 'realtime': t.realtime}];
-            });
-          }
-          
+          time.map(t => {
+
+            const dt = DateTime.fromSeconds(t.serviceDay);
+            let arrival = dt.plus({ seconds: t.realtimeArrival }).toLocaleString(DateTime.TIME_24_SIMPLE);
+            let departure = dt.plus({ seconds: t.realtimeDeparture }).toLocaleString(DateTime.TIME_24_SIMPLE);
+            let scheduleArr = dt.plus({ seconds: t.scheduledArrival }).toLocaleString(DateTime.TIME_24_SIMPLE);
+            let scheduleDep = dt.plus({ seconds: t.scheduledDeparture }).toLocaleString(DateTime.TIME_24_SIMPLE);
+            let timeDate = new Date(t.serviceDay * 1000).toUTCString();
+
+            this.timetable = [...this.timetable, { 'name': data.data.stop.name, 'headsign': t.headsign, 'realtimeArrival': t.realtimeArrival, 'realtimeDeparture': t.realtimeDeparture, 'scheduledArrival': scheduleArr, 'scheduledDeparture': scheduleDep, 'serviceDay': t.serviceDay, 'arrival': arrival, 'departure': departure, 'date': timeDate, 'realtime': t.realtime }];
+          });
+        }
+
       })
       .catch(error => {
         console.log(error);
